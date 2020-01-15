8fe77aa7b9d38252e8cab9b46e25ce06e25aa5b1
mattchley <mattchley@gmail.com>
2020-01-14 21:42:12 -0800

    bugFixforPost

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (username) VALUES ('Carlos');
+INSERT INTO user (username) VALUES ('Jennifer');
+INSERT INTO user (username) VALUES ('Evelyn');
+
+INSERT INTO movie (imdbID) VALUES ('tt0137523, fightclub');
+INSERT INTO movie (imdbID) VALUES ('tt0469494, therewillbeblood');
+INSERT INTO movie (imdbID) VALUES ('tt1798709, her');
+
+INSERT INTO review (giphy) VALUES ('https://media2.giphy.com/media/BlVnrxJgTGsUw/giphy.gif?cid=e16717f8138ea6fc667a35b6f382d5e9a378be40bcdcba5b&rid=giphy.gif');
+INSERT INTO review (giphy) VALUES ('https://media1.giphy.com/media/wACPZdg6nX52o/giphy.gif?cid=e16717f854bf85a005cf487c47d10ddba5732a0a5f9be718&rid=giphy.gif');
+INSERT INTO review (giphy) VALUES ('https://media3.giphy.com/media/9Y5BbDSkSTiY8/giphy.gif?cid=e16717f8367dc371330eb646fd35d9028457ad8bc0cf9402&rid=giphy.gif');
